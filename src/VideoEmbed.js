e345939bd68fa59081f6bd9f275866da78d4a269
rahulb1218 <65470932+rahulb1218@users.noreply.github.com>
2024-07-09 15:43:25 -0700

    update

--- src/VideoEmbed.js
+++ src/VideoEmbed.js
@@ -17,10 +17,15 @@ const VideoEmbed = () => {
   const videoId = getVideoId(decodedVideoLink);
 
   useEffect(() => {
-    if (window.YT) {
-      loadVideoPlayer();
-    } else {
+    if (!window.YT) {
+      const tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      const firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
       window.onYouTubeIframeAPIReady = loadVideoPlayer;
+    } else {
+      loadVideoPlayer();
     }
 
     function loadVideoPlayer() {
