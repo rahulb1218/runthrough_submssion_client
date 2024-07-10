27b5615688a88f35e173167a4f1daa8bb7657c53
rahulb1218 <65470932+rahulb1218@users.noreply.github.com>
2024-07-09 23:11:36 -0700

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   'http://localhost:3000',
   'https://boiling-sea-64676.herokuapp.com',
-  `${process.env.react_app_api_url}'
+  `${process.env.react_app_api_url}`
 ];
 
 app.use(cors({
